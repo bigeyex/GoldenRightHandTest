8c6841620edb43fa16e97f2a9d6861b2c2f8b3f2
GuoqiangXu <guoqiangxu@Guoqiang-XUs-MacBook-Pro.local>
2015-01-16 01:38:03 -0500

    adjust particle effect, monster move direction etc

--- RubberMan.spritebuilder/Source/Player.m
+++ RubberMan.spritebuilder/Source/Player.m
@@ -49,7 +49,7 @@ static float stopDuration = 0.3;
     _rightFoot.physicsBody.collisionCategories = @[@"hand"];
     
     // add hand into the scene
-    [self addHandwithName:@"Hand"];
+    [self addHandwithName:@"FireHand"];
     
     // set up initial parameters
     _playerHP = 100.0;
