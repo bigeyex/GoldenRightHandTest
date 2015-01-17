3f1e0ce0cb37c9a8bbd7112beff66960aa8f5042
Wang Yu <bigeyex@gmail.com>
2015-01-16 20:04:10 -0500

    fixed a bug from capitalization;
    and added Lifebar class

---
+++ RubberMan.spritebuilder/Source/LifeBar.h
@@ -0,0 +1,13 @@
+//
+//  LifeBar.h
+//  RubberMan
+//
+//  Created by Wang Yu on 1/16/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface LifeBar : CCNode
+
+@end
---
+++ RubberMan.spritebuilder/Source/LifeBar.m
@@ -0,0 +1,13 @@
+//
+//  LifeBar.m
+//  RubberMan
+//
+//  Created by Wang Yu on 1/16/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "LifeBar.h"
+
+@implementation LifeBar
+
+@end
